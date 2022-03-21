382faadf6f0ba723fd4ffa7703569910af1daadb
MarouanLaroui <marouanlaroui2000@gmail.com>
2022-03-21 16:19:27 +0100

    JSON to Weather
    Creation of a JSON to Weather function. Take the json from the API call and create a list of Weather objects.
    
    Modified getWeatherByName func, it is now using the newly created function

--- weather-app/src/app/services/weather/weather.service.ts
+++ weather-app/src/app/services/weather/weather.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { Weather } from 'src/app/models/Weather/weather';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,34 @@ import { Observable } from 'rxjs';
 export class WeatherService {
 
   constructor(private http : HttpClient) { }
+  
+  private jsonToWeathers(json : any) : Weather[]{
+    let weathers : Weather[] = []
 
-  getWeatherByName(cityName : string) : Observable<any>{
-    return this.http.get<any>(environment.openApiURL + `/data/2.5/forecast?q=${cityName}&limit=5&appid=${environment.openWeatherApiKey}`)
+    for(let i = 0; i<json.list.length;i++){
+      weathers.push(
+        new Weather(
+          json.list[i].weather[0].id,
+          json.list[i].weather[0].description,
+          json.list[i].weather[0].icon,
+          json.list[i].main.temp,
+          json.list[i].main.temp_min,
+          json.list[i].main.temp_max,
+          json.list[i].main.humidity,
+          json.list[i].wind.speed
+        )
+      )
+    }
+
+    return weathers
+
+  }
+
+  getWeatherByName(cityName : string) : Observable<Weather[]>{
+    return this.http.get<Weather[]>(environment.openApiURL + `/data/2.5/forecast?q=${cityName}&limit=5&appid=${environment.openWeatherApiKey}`)
+    .pipe(
+      map(json => this.jsonToWeathers(json)
+      ))
   }
 
   getWeatherByCoordinates(longitude : number, latitude : number): Observable<any>{
